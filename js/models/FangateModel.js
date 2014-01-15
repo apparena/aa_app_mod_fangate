2eb7fb454ea10b9638c3f143250c0d1ae274da64
Marcus Merchel <kontakt@marcusmerchel.de>
2014-01-15 14:19:27 +0100

    GNR-18 - optimize code with abstract code

--- js/models/FangateModel.js
+++ js/models/FangateModel.js
@@ -1,56 +1,29 @@
 define([
+    'ModelExtend',
     'underscore',
     'backbone',
     'localstorage'
-], function (_, Backbone) {
+], function (Model, _, Backbone) {
     'use strict';
 
-    var namespace = 'fangate',
-        Model, Init, Remove, Instance;
+    return function () {
+        Model.namespace = 'fangate';
 
-    Model = Backbone.Model.extend({
-        localStorage: new Backbone.LocalStorage('AppArenaAdventskalenderApp_' + _.aa.instance.aa_inst_id + '_Fan'),
+        Model.code = Backbone.Model.extend({
+            localStorage: new Backbone.LocalStorage('AppArenaAdventskalenderApp_' + _.aa.instance.aa_inst_id + '_Fan'),
 
-        defaults: {
-            isFan:         false,
-            isFacebookFan: false,
-            isTwitterFan:  false,
-            isGoogleFan:   false
-        },
+            defaults: {
+                isFan:         false,
+                isFacebookFan: false,
+                isTwitterFan:  false,
+                isGoogleFan:   false
+            },
 
-        initialize: function () {
-            this.fetch();
-        }
-    });
-
-    Remove = function () {
-        _.singleton.model[namespace].unbind().remove();
-        delete _.singleton.model[namespace];
-    };
-
-    Init = function (init) {
-
-        if (_.isUndefined(_.singleton.model[namespace])) {
-            _.singleton.model[namespace] = new Model();
-        } else {
-            if (!_.isUndefined(init) && init === true) {
-                Remove();
-                _.singleton.model[namespace] = new Model();
+            initialize: function () {
+                this.fetch();
             }
-        }
+        });
 
-        return _.singleton.model[namespace];
-    };
-
-    Instance = function () {
-        return _.singleton.model[namespace];
-    };
-
-    return {
-        init:        Init,
-        view:        Model,
-        remove:      Remove,
-        namespace:   namespace,
-        getInstance: Instance
-    };
+        return Model;
+    }
 });
--- js/views/FangateView.js
+++ js/views/FangateView.js
@@ -1,238 +1,210 @@
 define([
+    'ViewExtend',
     'jquery',
     'underscore',
     'backbone',
     'bootstrap',
     'modules/fangate/js/models/FangateModel',
     'text!modules/fangate/templates/fangate.html'
-], function ($, _, Backbone, Bootstrap, FangateModel, fangateTemplate) {
-
+], function (View, $, _, Backbone, Bootstrap, FangateModel, fangateTemplate) {
     'use strict';
 
-    var namespace = 'fangate',
-        View, Init, Remove, Instance;
+    return function () {
+        View.namespace = 'fangate';
 
-    View = Backbone.View.extend({
-        el: $('body'),
+        View.code = Backbone.View.extend({
+            el: $('body'),
 
-        events: {
-            'click .openfangate': 'openFangate'
-        },
+            events: {
+                'click .openfangate': 'openFangate'
+            },
 
-        initialize: function () {
-            _.bindAll(this, 'render', 'openFangate', 'socialButton', 'saveAsFan', 'changeButtonStyling');
+            initialize: function () {
+                _.bindAll(this, 'render', 'openFangate', 'socialButton', 'saveAsFan', 'changeButtonStyling');
 
-            // init fangate model to handle fan status
-            this.model = FangateModel.init();
-        },
+                // init fangate model to handle fan status
+                this.model = FangateModel().init();
+            },
 
-        render: function () {
-            /*
-             * very dirty implementation, because it didn't not work.
-             * maybe check:
-             * http://dailyjs.com/2012/12/06/backbone-tutorial-2/
-             * to use gplus with require/backbone...
-             */
-            /*var gpluscontent = (_.c('fangate_social_networks').indexOf('gplus') !== -1) ?
-             '<div class="g-follow" data-annotation="bubble" data-height="20" data-href="//plus.google.com/' + _.c('share_googleplus_id') + '" data-rel="author"></div>'
-             : '',
-             gplusscript = (_.c('fangate_social_networks').indexOf('gplus') !== -1) ?
-             '<script type="text/javascript">(function(){var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;po.src = "https://apis.google.com/js/plusone.js";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);})();</script>'
-             : '',
-             data = {gpluscontent: gpluscontent, gplusscript: gplusscript},
-             compiledTemplate = _.template(fangateTemplate, data);*/
+            render: function () {
+                /*
+                 * very dirty implementation, because it didn't not work.
+                 * maybe check:
+                 * http://dailyjs.com/2012/12/06/backbone-tutorial-2/
+                 * to use gplus with require/backbone...
+                 */
+                /*var gpluscontent = (_.c('fangate_social_networks').indexOf('gplus') !== -1) ?
+                 '<div class="g-follow" data-annotation="bubble" data-height="20" data-href="//plus.google.com/' + _.c('share_googleplus_id') + '" data-rel="author"></div>'
+                 : '',
+                 gplusscript = (_.c('fangate_social_networks').indexOf('gplus') !== -1) ?
+                 '<script type="text/javascript">(function(){var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;po.src = "https://apis.google.com/js/plusone.js";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);})();</script>'
+                 : '',
+                 data = {gpluscontent: gpluscontent, gplusscript: gplusscript},
+                 compiledTemplate = _.template(fangateTemplate, data);*/
 
-            var compiledTemplate = _.template(fangateTemplate, {});
-            this.$el.append(compiledTemplate);
-            //this.socialButton();
+                var compiledTemplate = _.template(fangateTemplate, {});
+                this.$el.append(compiledTemplate);
+                //this.socialButton();
 
-            return this;
-        },
+                return this;
+            },
 
-        openFangate: function () {
-            var that = this,
-                model = this.model,
-                fangate = $('#fangateModal');
+            openFangate: function () {
+                var that = this,
+                    model = this.model,
+                    fangate = $('#fangateModal');
 
-            // when we are on facebook, check the fb variable
-            if (typeof _.aa.fb.is_fb_user_fan === 'boolean') {
-                // reset fan status
-                model.set({
-                    'isFan':         false,
-                    'isFacebookFan': false
-                });
-                // check facebook fan status
-                if (_.aa.fb.is_fb_user_fan === true) {
-                    // ok user is facebook fan, save this
+                // when we are on facebook, check the fb variable
+                if (typeof _.aa.fb.is_fb_user_fan === 'boolean') {
+                    // reset fan status
                     model.set({
-                        'isFan':         true,
-                        'isFacebookFan': true
+                        'isFan':         false,
+                        'isFacebookFan': false
                     });
-                }
-                model.save();
-            }
-
-            // show fangate only if model is not defined or user is not fan
-            if (typeof model === 'undefined' || model.get('isFan') === false) {
-                // open modal without keyboard and backdrop click support
-                fangate.modal({keyboard: false, backdrop: 'static'});
-                // show social buttons
-                this.socialButton();
-                // change url to call fangate again if needed
-                this.goTo('call/fangate', false);
-
-                // log this action and count adminlog up
-                this.log('group', {
-                    app_fangate_open: {
-                        auth_uid:      _.uid,
-                        auth_uid_temp: _.uid_temp,
-                        code:          3001,
-                        data_obj:      {}
-                    },
-                    app_fangate_show: ''
-                });
-
-                fangate.on('hidden.bs.modal', function () {
-                    fangate.remove();
-                    that.goTo('', false);
-                    //Remove();
-                });
-
-                if (fangate.length === 0) {
-                    that.goTo('', false);
-                }
-            } else {
-                fangate.remove();
-                //Remove();
-            }
-
-            return this;
-        },
-
-        socialButton: function () {
-            var google, twitter, facebook,
-                that = this;
-
-            // facebook like button
-            if (_.c('fangate_social_networks').indexOf('fb') !== -1) {
-                require([
-                    'modules/facebook/js/views/FacebookView'
-                ], function (Facebook) {
-                    facebook = Facebook.init();
-
-                    facebook.libInit();
-                    facebook.like(function () {
-                        that.saveAsFan({
-                            target: {
-                                className: 'fangate_btn_facebook'
-                            }
+                    // check facebook fan status
+                    if (_.aa.fb.is_fb_user_fan === true) {
+                        // ok user is facebook fan, save this
+                        model.set({
+                            'isFan':         true,
+                            'isFacebookFan': true
                         });
+                    }
+                    model.save();
+                }
+
+                // show fangate only if model is not defined or user is not fan
+                if (typeof model === 'undefined' || model.get('isFan') === false) {
+                    // open modal without keyboard and backdrop click support
+                    fangate.modal({keyboard: false, backdrop: 'static'});
+                    // show social buttons
+                    this.socialButton();
+                    // change url to call fangate again if needed
+                    this.goTo('call/fangate', false);
+
+                    // log this action and count adminlog up
+                    this.log('group', {
+                        app_fangate_open: {
+                            auth_uid:      _.uid,
+                            auth_uid_temp: _.uid_temp,
+                            code:          3001,
+                            data_obj:      {}
+                        },
+                        app_fangate_show: ''
                     });
-                });
-            }
 
-            // google follow button
-            if (_.c('fangate_social_networks').indexOf('gplus') !== -1) {
-                require([
-                    'modules/google/js/views/GoogleView'
-                ], function (Google) {
-                    google = Google.init();
-                    google.libInit();
-                });
-            }
+                    fangate.on('hidden.bs.modal', function () {
+                        fangate.remove();
+                        that.goTo('', false);
+                        //Remove();
+                    });
 
-            // twitter follow button
-            if (_.c('fangate_social_networks').indexOf('twitter') !== -1) {
-                require([
-                    'modules/twitter/js/views/TwitterView'
-                ], function (Twitter) {
-                    twitter = Twitter.init();
+                    if (fangate.length === 0) {
+                        that.goTo('', false);
+                    }
+                } else {
+                    fangate.remove();
+                    //Remove();
+                }
 
-                    twitter.libInit();
-                    twitter.follow(function (response) {
+                return this;
+            },
+
+            socialButton: function () {
+                var google, twitter, facebook,
+                    that = this;
+
+                // facebook like button
+                if (_.c('fangate_social_networks').indexOf('fb') !== -1) {
+                    require([
+                        'modules/facebook/js/views/FacebookView'
+                    ], function (Facebook) {
+                        facebook = Facebook().init();
+
+                        facebook.libInit();
+                        facebook.like(function () {
                             that.saveAsFan({
                                 target: {
-                                    className: 'fangate_btn_twitter'
+                                    className: 'fangate_btn_facebook'
                                 }
                             });
-                        }
-                    );
-                });
+                        });
+                    });
+                }
+
+                // google follow button
+                if (_.c('fangate_social_networks').indexOf('gplus') !== -1) {
+                    require([
+                        'modules/google/js/views/GoogleView'
+                    ], function (Google) {
+                        google = Google().init();
+                        google.libInit();
+                    });
+                }
+
+                // twitter follow button
+                if (_.c('fangate_social_networks').indexOf('twitter') !== -1) {
+                    require([
+                        'modules/twitter/js/views/TwitterView'
+                    ], function (Twitter) {
+                        twitter = Twitter().init();
+
+                        twitter.libInit();
+                        twitter.follow(function (response) {
+                                that.saveAsFan({
+                                    target: {
+                                        className: 'fangate_btn_twitter'
+                                    }
+                                });
+                            }
+                        );
+                    });
+                }
+
+                return this;
+            },
+
+            saveAsFan: function (element) {
+                var data = {};
+
+                switch (element.target.className) {
+                    case 'fangate_btn_facebook':
+                        data = {
+                            isFan:         true,
+                            isFacebookFan: true
+                        };
+                        break;
+                    case 'fangate_btn_google':
+                        data = {
+                            isFan:       true,
+                            isGoogleFan: true
+                        };
+                        break;
+                    case 'fangate_btn_twitter':
+                        data = {
+                            isFan:        true,
+                            isTwitterFan: true
+                        };
+                        break;
+                }
+
+                this.model.set(data);
+                this.model.save();
+
+                this.changeButtonStyling();
+
+                return this;
+            },
+
+            changeButtonStyling: function () {
+                this.$el
+                    .find('#fangateModal .modal-footer a')
+                    .addClass('btn btn-primary')
+                    .html(_.c('close_fangate_highlighted'));
+
+                return this;
             }
+        });
 
-            return this;
-        },
-
-        saveAsFan: function (element) {
-            var data = {};
-
-            switch (element.target.className) {
-                case 'fangate_btn_facebook':
-                    data = {
-                        isFan:         true,
-                        isFacebookFan: true
-                    };
-                    break;
-                case 'fangate_btn_google':
-                    data = {
-                        isFan:       true,
-                        isGoogleFan: true
-                    };
-                    break;
-                case 'fangate_btn_twitter':
-                    data = {
-                        isFan:        true,
-                        isTwitterFan: true
-                    };
-                    break;
-            }
-
-            this.model.set(data);
-            this.model.save();
-
-            this.changeButtonStyling();
-
-            return this;
-        },
-
-        changeButtonStyling: function () {
-            this.$el
-                .find('#fangateModal .modal-footer a')
-                .addClass('btn btn-primary')
-                .html(_.c('close_fangate_highlighted'));
-
-            return this;
-        }
-    });
-
-    Remove = function () {
-        _.singleton.view[namespace].unbind().remove();
-        delete _.singleton.view[namespace];
-    };
-
-    Init = function (init) {
-
-        if (_.isUndefined(_.singleton.view[namespace])) {
-            _.singleton.view[namespace] = new View();
-        } else {
-            if (!_.isUndefined(init) && init === true) {
-                Remove();
-                _.singleton.view[namespace] = new View();
-            }
-        }
-
-        return _.singleton.view[namespace];
-    };
-
-    Instance = function () {
-        return _.singleton.view[namespace];
-    };
-
-    return {
-        init:        Init,
-        view:        View,
-        remove:      Remove,
-        namespace:   namespace,
-        getInstance: Instance
-    };
+        return View;
+    }
 });
